9d3ddd8e60fb65c9f03d41cae4261aeef32edb0d
Paul Clark <systemslibrarian@gmail.com>
2025-02-21 16:01:07 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 echo "==========================="
 
-git config --global user.name "${GITHUB_AUTHOR}"
-git config --global user.email "${INPUT_EMAIL}"
+#git config --global user.name "${GITHUB_AUTHOR}"
+#git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
 python /usr/bin/feed.py
