1ade3f17faed59402f031566b62111baddf0826e
Paul Clark <systemslibrarian@gmail.com>
2025-02-21 15:58:52 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
 python /usr/bin/feed.py
-git add -A && commit -m "Update Feed"
+git add -A && git commit -m "Update Feed"
 git push --set-upstream origin main
 
 echo "==========================="
