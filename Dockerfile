c698748f1a9be9fbe5aff1a630550c8e2166254c
Paul Clark <systemslibrarian@gmail.com>
2025-02-21 20:23:35 +0000

    Updated project with necessary files

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,14 @@
 FROM ubuntu:latest
 
-RUN 
+RUN apt-get update && apt-get install -y \
+    Python3.10 \
+    python3-pip \
+    git
+
+RUN pip3 install pyYAML
+
+COPY feed.py /usr/bin/feed.py  
+
+copy entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "==========================="
+
+git config --global user.name "${GITHUB_AUTHOR}"
+git config --global user.email "${INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
+
+python /usr/bin/feed.py
+git add -A && commit -m "Update Feed"
+git push --set-upstream origin main
+
+echo "==========================="
